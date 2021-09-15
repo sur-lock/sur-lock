10cde1b39a1f794bcbf0235e63063ab6b7a776e8
Dongkyun_Jang <thd6576@naver.com>
2021-09-15 13:54:02 +0900

    feat: 새롭게 정의한 theme을 적용시킵니다

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,27 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import { GlobalStyle } from "style/GlobalStyle";
+import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { MainPage } from "pages";
+import { darkTheme, lightTheme } from "style/theme";
 
 export default function App(): ReactElement {
+	const [theme, setTheme] = useState(lightTheme);
+
+	const switchTheme = () => {
+		const nextTheme = theme === lightTheme ? darkTheme : lightTheme;
+		setTheme(nextTheme);
+	};
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<GlobalStyle />
+			{/* <Header switchTheme={switchTheme} /> */}
 			<Router>
 				<Switch>
 					<Route exact path="/" component={MainPage} />
 				</Switch>
 			</Router>
-		</>
+			{/* <Footer /> */}
+		</ThemeProvider>
 	);
 }
