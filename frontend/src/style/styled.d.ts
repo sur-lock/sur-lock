4c10c40cae103793fbaa2f69f9edfe4d31fc3650
Dongkyun_Jang <thd6576@naver.com>
2021-09-15 13:52:24 +0900

    feat: 새롭게 추가된 style에 대한 타입을 정의합니다

---
+++ frontend/src/style/styled.d.ts
@@ -0,0 +1,51 @@
+import { FlattenSimpleInterpolation } from "styled-components";
+
+declare module "styled-components" {
+	export interface DefaultTheme {
+		margins: {
+			sm: string;
+			base: string;
+			lg: string;
+			xl: string;
+		};
+		paddings: {
+			sm: string;
+			base: string;
+			lg: string;
+			xl: string;
+		};
+		fonts: {
+			family: {
+				base: string;
+				title: string;
+			};
+			size: {
+				sm: string;
+				base: string;
+				lg: string;
+				xl: string;
+				title: string;
+			};
+			weight: {
+				light: number;
+				normal: number;
+				bold: number;
+			};
+		};
+		device: {
+			mobile: string;
+			tablet: string;
+			desktop: string;
+		};
+		display: {
+			flexCol: (
+				justifyContent?: string,
+				alignItems?: string,
+			) => FlattenSimpleInterpolation;
+			flexRow: (
+				justifyContent?: string,
+				alignItems?: string,
+			) => FlattenSimpleInterpolation;
+		};
+	}
+}
