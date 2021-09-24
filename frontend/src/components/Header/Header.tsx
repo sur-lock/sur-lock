dd7982ada8833b61e6bf5bb5b961ee29f375680c
Dongkyun_Jang <thd6576@naver.com>
2021-09-24 11:49:09 +0900

    chore: 토글 버튼을 별개의 컴포넌트로 분리

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import ThemeToggle from "./ThemeToggle";
+import ThemeToggle from "../ThemeToggle";
 
 interface HeaderProps {
 	switchTheme: () => void;
