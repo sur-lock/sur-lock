4e6f30f36d638bd7a5c2e4c6b8032064fee1bacf
Minho Jang <minho9301@naver.com>
2021-09-27 17:10:30 +0900

    feat: 응답 이미지에 적절한 Content-Type 설정

--- backend/src/main/java/com/surlock/api/ImageRestController.java
+++ backend/src/main/java/com/surlock/api/ImageRestController.java
@@ -1,11 +1,15 @@
 package com.surlock.api;
 
+import com.surlock.common.util.MimeMap;
 import com.surlock.entity.Image;
 import com.surlock.exception.NotFoundException;
 import com.surlock.service.ImageService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -23,7 +27,7 @@ public class ImageRestController {
     ImageService imageService;
 
     @GetMapping(value = "{image_name}")
-    public byte[] getImage(@PathVariable("image_name") String imageName) throws IOException {
+    public ResponseEntity<byte[]> getImage(@PathVariable("image_name") String imageName) throws IOException {
         log.info("GET /images/" + imageName);
 
         Image target = imageService.getImageByName(imageName);
@@ -36,7 +40,10 @@ public class ImageRestController {
 
         InputStream in = getClass().getResourceAsStream(imagePath);
         if (in != null) {
-            return IOUtils.toByteArray(in);
+            HttpHeaders headers = new HttpHeaders();
+            String mime = MimeMap.findMimeByExt(target.getExt());
+            headers.add("Content-Type", mime);
+            return new ResponseEntity<>(IOUtils.toByteArray(in), headers, HttpStatus.OK);
         } else {
             throw new NotFoundException("Can't access this file : " + imagePath);
         }
