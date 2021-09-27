acd1ca3671b2eba2cd62380899ddabf4c4d9cd0f
Minho Jang <minho9301@naver.com>
2021-09-27 15:46:37 +0900

    feat: 이미지 관련 서비스, 레포지토리, 컨트롤러 (미완)

---
+++ backend/src/main/java/com/surlock/api/ImageRestController.java
@@ -0,0 +1,44 @@
+package com.surlock.api;
+
+import com.surlock.entity.Image;
+import com.surlock.exception.NotFoundException;
+import com.surlock.service.ImageService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/images")
+@Slf4j
+public class ImageRestController {
+
+    private static final String IMAGE_BASE_PATH = "/images/";
+
+    @Autowired
+    ImageService imageService;
+
+    @GetMapping(value = "{image_name}")
+    public byte[] getImage(@PathVariable("image_name") String imageName) throws IOException {
+        log.info("GET /images/" + imageName);
+
+        Image target = imageService.getImageByName(imageName);
+        String imagePath = IMAGE_BASE_PATH +
+                target.getImageName() +
+                "." +
+                target.getExt();
+
+        log.info("Image path : " + imagePath);
+
+        InputStream in = getClass().getResourceAsStream(imagePath);
+        if (in != null) {
+            return IOUtils.toByteArray(in);
+        } else {
+            throw new NotFoundException("Can't access this file : " + imagePath);
+        }
+    }
+}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO images (image_id, create_at, ext, image_name) VALUES (1, current_timestamp(), 'jpg', 'test');
