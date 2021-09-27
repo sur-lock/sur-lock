bc69ee25891899196c07fc175f302dba4d8eae0b
Minho Jang <minho9301@naver.com>
2021-09-27 17:08:46 +0900

    feat: hibernate에서 데이터 초기화를 위해 import.sql 생성

--- backend/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO images (image_id, create_at, ext, image_name) VALUES (1, current_timestamp(), 'jpg', 'test');
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO images (image_id, create_at, ext, image_name) VALUES (1, current_timestamp(), 'jpg', 'test'), (2, current_timestamp(), 'png', 'java-icon');
