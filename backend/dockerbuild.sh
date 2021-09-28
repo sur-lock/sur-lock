f90b6b768294a9852bc4a28a8aec015b2b0bb2ad
Minho Jang <minho9301@naver.com>
2021-09-28 22:27:02 +0900

    fix: 공유 디렉터리 경로 수정

--- backend/dockerbuild.sh
+++ backend/dockerbuild.sh
@@ -1,4 +1,4 @@
 docker build -t surlock ./
 docker ps -f name=surlock  -q | xargs --no-run-if-empty docker container stop
 docker container ls -a -f name=surlock -q | xargs -r docker container rm
-docker run -d -p 8080:8080 --name surlock surlock --mount type=bind,source=/home/ubuntu/upload-images,target=/upload-images
+docker run -d -p 8080:8080 --name surlock surlock --mount type=bind,source=/home/ubuntu/upload-images,target=/tmp/upload-images
