fe6a797b340151130ad2a4441f06a1683eac3fb0
Minho Jang <minho9301@naver.com>
2021-09-28 13:47:13 +0900

    fix: mime 관련 유틸 이름 변경

---
+++ backend/src/main/java/com/surlock/common/util/ExtMimeUtils.java
@@ -0,0 +1,27 @@
+package com.surlock.common.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExtMimeUtils {
+    private static final Map<String, String> extMimeMap;
+    private static final String defaultMime = "image/jpeg";
+
+    static {
+        extMimeMap = new HashMap<>();
+        extMimeMap.put("gif", "image/gif");
+        extMimeMap.put("jpg", "image/jpeg");
+        extMimeMap.put("jpeg", "image/jpeg");
+        extMimeMap.put("svg", "image/svg+xml");
+        extMimeMap.put("tif", "image/tiff");
+        extMimeMap.put("tiff", "image/tiff");
+        extMimeMap.put("webp", "image/webp");
+        extMimeMap.put("png", "image/png");
+        extMimeMap.put("bmp", "image/bmp");
+        extMimeMap.put("heic", "image/heic");
+    }
+
+    public static String findMimeByExt(String ext) {
+        return extMimeMap.getOrDefault(ext, defaultMime);
+    }
+}
