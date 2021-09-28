d22986b2827fac18728af30a1f40512cec6f28d4
chaeha <chaeha93@gmail.com>
2021-09-28 13:50:43 +0900

    feat: docker_build

---
+++ backend/dockerbuild.sh
@@ -0,0 +1,4 @@
+docker build -t surlock ./
+docker ps -f name=surlock  -q | xargs --no-run-if-empty docker container stop
+docker container ls -a -f name=surlock -q | xargs -r docker container rm
+docker run -d -p 8080:8080 --name surlock surlock
