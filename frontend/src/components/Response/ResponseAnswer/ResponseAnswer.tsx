9c0ed21d24f9eb80f61dfaec225699c54fbef0d6
신주환 <jason-shin94@hanmail.net>
2021-10-02 17:27:38 +0900

    feat:주관식 질문 컴포넌트

--- frontend/src/components/Response/ResponseAnswer/ResponseAnswer.tsx
+++ frontend/src/components/Response/ResponseAnswer/ResponseAnswer.tsx
@@ -1,6 +1,54 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import "antd/dist/antd.css";
+import { Card, Form, Input } from "antd";
 
-export function ResponseAnswer() {
-	return <div>11</div>;
+interface questionData {
+	QuestionIdx: number;
+	title: string;
+	sendData: (idx: number, data: any) => void;
 }
+
+export function ResponseAnswer({ QuestionIdx, title, sendData }: questionData) {
+	const [Answer, setAnswer] = useState("");
+
+	const onAnswerChangeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+		setAnswer(e.currentTarget.value);
+		sendData(QuestionIdx, Answer);
+	};
+
+	return (
+		<SurveyForm>
+			<Form layout="vertical" autoComplete="off" size="large">
+				<FormWrapper style={{ overflow: "hidden" }}>
+					<Form.Item name="SurveyTitle">
+						<Label>{title}</Label>
+						<Input
+							onChange={onAnswerChangeHandler}
+							placeholder="답변을 입력해주세요"
+						/>
+					</Form.Item>
+				</FormWrapper>
+			</Form>
+		</SurveyForm>
+	);
+}
+
+const Label = styled.h1`
+	font-size: 3rem;
+	float: left;
+	margin-bottom: 10px;
+	coler: #fff;
+	span {
+		display: block;
+	}
+`;
+const FormWrapper = styled.div`
+	font-size: 2rem;
+`;
+
+const SurveyForm = styled(Card)`
+	width: 65%;
+	padding: 20px;
+	margin: 20px;
+`;
--- frontend/src/pages/ResponseSurvey/ResponseSurvey.tsx
+++ frontend/src/pages/ResponseSurvey/ResponseSurvey.tsx
@@ -7,7 +7,12 @@ import {
 	PictureOutlined,
 	UploadOutlined,
 } from "@ant-design/icons";
-import { ResponseOption, ResponseOptionWithImg } from "components";
+import {
+	ResponseOption,
+	ResponseOptionWithImg,
+	ResponseAnswer,
+	ResponseAnswerWithImg,
+} from "components";
 import "antd/dist/antd.css";
 import { Card } from "antd";
 import { Fab, Action } from "react-tiny-fab";
@@ -64,6 +69,11 @@ export function ResponseSurvey() {
 				title={sampleData.questions[2].title}
 				imgs={sampleData.questions[2].imgs}
 				QuestionIdx={2}
+				sendData={getDatafromChild}
+			/>
+			<ResponseAnswer
+				title={sampleData.questions[3].title}
+				QuestionIdx={3}
 				sendData={getDatafromChild}
 			/>
 		</CardContainer>
