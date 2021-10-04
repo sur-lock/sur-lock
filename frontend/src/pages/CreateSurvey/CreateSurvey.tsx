191a6185092122c88ffe5e3e4000cff673d5cf6e
신주환 <jason-shin94@hanmail.net>
2021-10-05 04:14:36 +0900

    feat : 설문조사 블록체인에 등록

--- frontend/src/pages/CreateSurvey/CreateSurvey.tsx
+++ frontend/src/pages/CreateSurvey/CreateSurvey.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { ethers } from "ethers";
 import {
 	CheckOutlined,
 	FileAddOutlined,
@@ -27,15 +28,245 @@ export function CreateSurvey() {
 		endDate: "",
 		title: "",
 		discription: "",
-		questions: [{ qType: 0, title: "", options: [""], imgs: [""] }],
+		questions: [{ qType: "none", title: "", options: [""], imgs: [""] }],
 	};
 	const [QuestionCount, setQuestionCount] = useState(0);
 	const [Survey, setSurvey] = useState(initialState);
+	const surlockCA = "0x1B57F6A4c67cC0961aF90c3B4c1b599c8bD97759";
+	const infuraProvider = new ethers.providers.InfuraProvider(
+		"ropsten",
+		"6d88878e015047cbad527b89ae00e284",
+	);
+	const surveyKey = "survey1";
+	const private_key =
+		"0x9292c97646451b2a9540062e8ceb465f61cb29f17d0e55c116c95051049f54bd";
+	const abi = [
+		{
+			inputs: [
+				{
+					internalType: "string",
+					name: "_respondent",
+					type: "string",
+				},
+				{
+					internalType: "string",
+					name: "_key",
+					type: "string",
+				},
+				{
+					internalType: "string[]",
+					name: "_response",
+					type: "string[]",
+				},
+			],
+			name: "addResponse",
+			outputs: [],
+			stateMutability: "nonpayable",
+			type: "function",
+		},
+		{
+			inputs: [
+				{
+					internalType: "string",
+					name: "_creator",
+					type: "string",
+				},
+				{
+					internalType: "string",
+					name: "_key",
+					type: "string",
+				},
+				{
+					internalType: "string",
+					name: "_startDate",
+					type: "string",
+				},
+				{
+					internalType: "string",
+					name: "_endDate",
+					type: "string",
+				},
+				{
+					components: [
+						{
+							internalType: "string",
+							name: "qType",
+							type: "string",
+						},
+						{
+							internalType: "string",
+							name: "title",
+							type: "string",
+						},
+						{
+							internalType: "string[]",
+							name: "options",
+							type: "string[]",
+						},
+						{
+							internalType: "string[]",
+							name: "imgs",
+							type: "string[]",
+						},
+					],
+					internalType: "struct SurLock.Question[]",
+					name: "_questions",
+					type: "tuple[]",
+				},
+			],
+			name: "addSurvey",
+			outputs: [],
+			stateMutability: "nonpayable",
+			type: "function",
+		},
+		{
+			inputs: [
+				{
+					internalType: "string",
+					name: "_key",
+					type: "string",
+				},
+			],
+			name: "getResponses",
+			outputs: [
+				{
+					internalType: "string[][]",
+					name: "",
+					type: "string[][]",
+				},
+			],
+			stateMutability: "view",
+			type: "function",
+		},
+		{
+			inputs: [
+				{
+					internalType: "string",
+					name: "_key",
+					type: "string",
+				},
+			],
+			name: "getSurvey",
+			outputs: [
+				{
+					components: [
+						{
+							internalType: "string",
+							name: "creator",
+							type: "string",
+						},
+						{
+							internalType: "string",
+							name: "startDate",
+							type: "string",
+						},
+						{
+							internalType: "string",
+							name: "endDate",
+							type: "string",
+						},
+						{
+							components: [
+								{
+									internalType: "string",
+									name: "qType",
+									type: "string",
+								},
+								{
+									internalType: "string",
+									name: "title",
+									type: "string",
+								},
+								{
+									internalType: "string[]",
+									name: "options",
+									type: "string[]",
+								},
+								{
+									internalType: "string[]",
+									name: "imgs",
+									type: "string[]",
+								},
+							],
+							internalType: "struct SurLock.Question[]",
+							name: "questions",
+							type: "tuple[]",
+						},
+						{
+							internalType: "string[][]",
+							name: "responses",
+							type: "string[][]",
+						},
+						{
+							internalType: "string[]",
+							name: "respondents",
+							type: "string[]",
+						},
+					],
+					internalType: "struct SurLock.Survey",
+					name: "",
+					type: "tuple",
+				},
+			],
+			stateMutability: "view",
+			type: "function",
+		},
+	];
 
+	async function getSurvey() {
+		if (!surveyKey) {
+			console.error("No entered key of survey. Please enter the key.");
+			return;
+		}
+
+		if (infuraProvider) {
+			console.log({ infuraProvider });
+			const contract = new ethers.Contract(surlockCA, abi, infuraProvider);
+			console.log(contract);
+
+			try {
+				const data = await contract.getSurvey("surveyTest2");
+				console.log("data: ", data);
+			} catch (err) {
+				console.log("Error: ", err);
+			}
+		}
+	}
+	async function addSurvey() {
+		if (infuraProvider) {
+			const wallet = new ethers.Wallet(private_key, infuraProvider);
+
+			const contract = new ethers.Contract(surlockCA, abi, wallet);
+			const sendedData: any[] = [];
+			for (let i = 0; i < Survey.questions.length; i += 1) {
+				const tempQuestion: any[] = [];
+				tempQuestion.push(Survey.questions[i].qType);
+				tempQuestion.push(Survey.questions[i].title);
+				tempQuestion.push(Survey.questions[i].options);
+				tempQuestion.push(Survey.questions[i].imgs);
+				sendedData.push(tempQuestion);
+			}
+			console.log(sendedData);
+			const transaction = await contract.addSurvey(
+				"888888",
+				"surveyTest2",
+				"2021-09-29T00:00:00",
+				"2021-10-29T23:59:59",
+				sendedData,
+			);
+
+			await transaction.wait();
+		}
+	}
+	const onSubmit = () => {
+		// addSurvey();
+		// console.log("####################");
+		getSurvey();
+	};
 	const onAddOptional = () => {
 		const newSurvey = Survey;
 		newSurvey.questions.push({
-			qType: 1,
+			qType: "select",
 			title: "",
 			options: [""],
 			imgs: [""],
@@ -46,7 +277,7 @@ export function CreateSurvey() {
 	const onAddOptionalWithImage = () => {
 		const newSurvey = Survey;
 		newSurvey.questions.push({
-			qType: 2,
+			qType: "selectImg",
 			title: "",
 			options: [""],
 			imgs: [""],
@@ -57,7 +288,7 @@ export function CreateSurvey() {
 	const onAddAnswer = () => {
 		const newSurvey = Survey;
 		newSurvey.questions.push({
-			qType: 3,
+			qType: "write",
 			title: "",
 			options: [""],
 			imgs: [""],
@@ -68,7 +299,7 @@ export function CreateSurvey() {
 	const onAddAnswerWithImage = () => {
 		const newSurvey = Survey;
 		newSurvey.questions.push({
-			qType: 4,
+			qType: "writeImg",
 			title: "",
 			options: [""],
 			imgs: [""],
@@ -78,16 +309,16 @@ export function CreateSurvey() {
 	};
 
 	const getDatafromChild = (index: number, val: any) => {
-		if (Survey.questions[index].qType === 0) {
+		if (Survey.questions[index].qType === "none") {
 			const newSurvey = Survey;
 			newSurvey.title = val.title;
 			newSurvey.discription = val.disc;
 			setSurvey(Survey => newSurvey);
-		} else if (Survey.questions[index].qType === 3) {
+		} else if (Survey.questions[index].qType === "write") {
 			const newSurvey = Survey;
 			newSurvey.questions[index].title = val;
 			setSurvey(Survey => newSurvey);
-		} else if (Survey.questions[index].qType === 4) {
+		} else if (Survey.questions[index].qType === "writeImg") {
 			const newSurvey = Survey;
 			newSurvey.questions[index].title = val.ans;
 			newSurvey.questions[index].imgs[0] = val.src;
@@ -103,23 +334,23 @@ export function CreateSurvey() {
 	const renderSurveys = () => {
 		const result = [];
 		for (let i = 0; i <= QuestionCount; i += 1) {
-			if (Survey.questions[i].qType === 0) {
+			if (Survey.questions[i].qType === "none") {
 				result.push(
 					<SurveyTitle QuestionIdx={i} sendData={getDatafromChild} />,
 				);
-			} else if (Survey.questions[i].qType === 1) {
+			} else if (Survey.questions[i].qType === "select") {
 				result.push(
 					<OptionalSurvey QuestionIdx={i} sendData={getDatafromChild} />,
 				);
-			} else if (Survey.questions[i].qType === 2) {
+			} else if (Survey.questions[i].qType === "selectImg") {
 				result.push(
 					<OptionalSurveyWithImg QuestionIdx={i} sendData={getDatafromChild} />,
 				);
-			} else if (Survey.questions[i].qType === 3) {
+			} else if (Survey.questions[i].qType === "write") {
 				result.push(
 					<AnswerSurvey QuestionIdx={i} sendData={getDatafromChild} />,
 				);
-			} else if (Survey.questions[i].qType === 4) {
+			} else if (Survey.questions[i].qType === "writeImg") {
 				result.push(
 					<AnswerSurveyWithImg QuestionIdx={i} sendData={getDatafromChild} />,
 				);
@@ -133,7 +364,7 @@ export function CreateSurvey() {
 			<Title>설문조사 생성</Title>
 			{renderSurveys()}
 			<Fab icon="+">
-				<Action text="제출">
+				<Action text="제출" onClick={onSubmit}>
 					<UploadOutlined />
 				</Action>
 				<Action text="객관식" onClick={onAddOptional}>
